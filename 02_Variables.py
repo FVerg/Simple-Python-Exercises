3d681cd34e47edbfa87d9d5c7e8e5981fa535527
FVerg <vergonaf@gmail.com>
2017-07-21 13:27:10 +0200

    02_Variables.py Created. Variables understanding.

---
+++ 02_Variables.py
@@ -0,0 +1,16 @@
+# Very simple users, used to understand the usage of variables
+
+greetingVar = "Hello World"
+
+print ("The following line will be printed by using variables: ")
+
+print (greetingVar)
+
+# We want now to get the input by the user
+
+userInput = input ("Write something: ")
+
+print ("The user typed: ")
+print (userInput);
+
+# Variables cannot start with a number
