dad08e064de1f2badbcd870d14b445b270e08886
FVerg <vergonaf@gmail.com>
2017-07-23 21:07:59 +0200

    User Input and While loops added

---
+++ 14_User_Input.py
@@ -0,0 +1,5 @@
+# In this lecture we are going to cover the user interaction with the program.
+
+planet = input ("Which planet are you from?")
+
+# This way we're going to put the user typed value into planet variable
