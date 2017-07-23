d4abb9387a6cb840c64105bb1b71e1541ff73286
FVerg <vergonaf@gmail.com>
2017-07-23 10:38:10 +0200

    Functions fundamentals added

---
+++ 10_Functions.py
@@ -0,0 +1,40 @@
+# In this exercise we'll cover the usage of custom functions
+# In the previous lectures, we've already used a lot of functions, but they
+# were already defined by Python itself (print, dir, upper, etc.)
+
+# In this exercise we're going to define our custom functions
+
+# Let's assume we want to create a function which converts dollars to euros
+
+# It will take 2 parameters:
+# 1. Rate: The exchange rate from euros to dollars
+# 2. Euros: The amount of euros to convert
+# The parameters represent the input of the function
+
+def currency_converter (rate, euros):
+    dollars = float(euros)*rate
+    return dollars
+
+# Some things to notice:
+# - The name of the function is the one following the keyword def: currency_converter
+# - The code of the function is specified in the lines after the ":", and must be
+#   written with at least one space from the beginning of the line. Otherwise
+#   Python won't understand that the code belongs to the function we are defining.
+# - The parameters (the ones inside the "()" brackets) specify an input for the function.
+# - The return keyword allows the function to specify an output.
+# - So, the function currency_converter has:
+#     - INPUT: rate, euros
+#     - OUTPUT: dollars
+# - In consequence of what we just said, INDENTATION IS CRUCIAL in Python
+
+# In order to use the function, we have to call it
+
+input_euros = input ("Insert the amount of euros to convert:")
+
+# In the following line we are calling the function currency_converter and
+# we are passing it the parameters:
+# rate = 1.8
+# euros = input_euros (Inserted by the user)
+output_dollars = currency_converter(1.18, input_euros)
+
+print ("The corresponding amount in dollars is: ", output_dollars)
