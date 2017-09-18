f143cda53d827a67632aeb62e404bede75e1afe2
FVerg <vergonaf@gmail.com>
2017-09-19 00:11:07 +0200

    Added final version of the map.

--- A02_Web_Map/A02_Web_Map.py
+++ A02_Web_Map/A02_Web_Map.py
@@ -185,3 +185,59 @@ for latitude, longitude, el in zip (lat, lon, elev):
 map.add_child(fg)
 
 map.save("Output/Map8.html")
+
+# We are now going to add a third layer. By now, our map has been equipped with
+# two different layers: 1) The base layer, representing the map itself
+# 2) The upper layer, containing the markers
+
+# We want to add a third layer: a polygon layer.
+# The layer we want to add is the one representing the population of each country
+# Each country will have a specific color representing its population.
+
+# To add a polygon we need to use the json.GeoJson
+map = folium.Map(location=[43,13], tiles="Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My markers")
+
+for latitude, longitude, el in zip (lat, lon, elev):
+    color = calculate_color(el)
+    fg.add_child(folium.CircleMarker(location=[latitude,longitude], popup=str(el)+" m", color = color, fill = True, radius=2))
+
+# The layer we need to add is a GeoJson: a JSON file
+# It is included in the Resources folder (world.json)
+# The parameter data takes a file which we first need to import in python
+# data: File containing the layer informations
+# encoding: Type of character encoding
+# style_function: We pass a lambda function (anonymous fucntion)
+#                 and indicate that the color of the polygon has to be green (fillColor)
+#                 if the population is lower than 1mln. The population number can be
+#                 retrieved by the json file, in the attribute POP2005. The JSON file has
+#                 a dictionary called properties, in which it stores some properties, as population.
+#                 In style_function, the parameter x represents 'features'
+fg. add_child(folium.GeoJson(data=open("Resources/world.json", 'r', encoding='utf-8-sig').read(), overlay = True,
+style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange'
+if 10000000 <= x['properties']['POP2005'] <20000000 else 'red'}))
+map.add_child(fg)
+
+map.save("Output/Map9.html")
+
+# The last thing we want to add is a control panel for all our layers
+# We need the folium.LayerControl class
+
+map = folium.Map(location=[43,13], tiles="Mapbox Bright")
+
+fg_volcanoes = folium.FeatureGroup(name="Volcanoes")
+fg_population = folium.FeatureGroup(name="Population")
+for latitude, longitude, el in zip (lat, lon, elev):
+    color = calculate_color(el)
+    fg_volcanoes.add_child(folium.CircleMarker(location=[latitude,longitude], popup=str(el)+" m", color = color, fill = True, radius=2))
+
+fg_population. add_child(folium.GeoJson(data=open("Resources/world.json", 'r', encoding='utf-8-sig').read(), overlay = True,
+style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange'
+if 10000000 <= x['properties']['POP2005'] <20000000 else 'red'}))
+
+map.add_child(fg_volcanoes)
+map.add_child(fg_population)
+map.add_child(folium.LayerControl())
+
+map.save("Output/Map10.html")
